4385a7b6454f2d1ee534d3acf3ca6f42d67f1619
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-10-18 12:28:13 +0100

    create CVSProcesser to check validity of a file

--- src/main/java/uk/gov/companieshouse/filevalidationservice/parser/CsvProcessor.java
+++ src/main/java/uk/gov/companieshouse/filevalidationservice/parser/CsvProcessor.java
@@ -1,6 +1,8 @@
 package uk.gov.companieshouse.filevalidationservice.parser;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -8,58 +10,60 @@ import java.util.List;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import uk.gov.companieshouse.filevalidationservice.utils.StaticPropertyUtil;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 public class CsvProcessor {
 
-    private static final int INDEX_OF_UNIQUE_ID = 0;
-    private static final int INDEX_OF_REGISTERED_COMPANY_NAME = 1;
-    private static final int INDEX_OF_COMPANY_NUMBER = 2;
-    private static final int INDEX_OF_TRADING_NAME = 3;
-    private static final int INDEX_OF_FIRST_NAME = 4;
-    private static final int INDEX_OF_LAST_NAME = 5;
-    private static final int INDEX_OF_DATE_OF_BIRTH = 6;
+
+    private static final  List<String> VALID_HEADERS = List.of("UniqueID", "Registered Company Name", "Company Number", "Trading Name", "First Name", "Last Name", "Date of Birth");
+    private static final Logger LOGGER = LoggerFactory.getLogger( StaticPropertyUtil.APPLICATION_NAMESPACE );
 
     private final Reader reader;
-    private boolean successfullyProcessedSoFar = true;
-    private int currentRecordBeingParsed = -1;
-
-    private static final String NULL_FIELD = "-";
 
 
-    public CsvProcessor(Reader reader) {
-        this.reader = reader;
+
+    public CsvProcessor( byte[] bytesToParse) {
+        ByteArrayInputStream decodedBase64AsStream = new ByteArrayInputStream(bytesToParse);
+        reader = new InputStreamReader(decodedBase64AsStream);
     }
 
     public boolean parseRecords() throws IOException {
-        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withNullString(NULL_FIELD).parse(reader);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(reader);
         Iterator<CSVRecord> it = records.iterator();
+        CSVRecord headers = records.iterator().next();
         try {
-            if (movePastHeadersToStartOfData(it, INITIAL_HEADER_RECORDS_TO_IGNORE)) {
-                while (it.hasNext()) {
-                    currentRecordBeingParsed++;
-                    parseRecord(it.next());
-                }
-            } else {
-                successfullyProcessedSoFar = false;
-            }
+            return isValidFieldHeaders(headers) && isDataAfterHeaders(it);
         } catch (IllegalStateException ex) {
-            LOG.error("Error parsing, could be corrupt CSV, at record number ([{}]: {}",
-                    currentRecordBeingParsed, ex, ex);
-            successfullyProcessedSoFar = false;
+            LOGGER.error("Error parsing, could be corrupt CSV, at record number" + ex);
+            return false;
         } finally {
             reader.close();
         }
-        return successfullyProcessedSoFar;
     }
 
 
-    public Boolean validateFieldHeaders (List<String> csvFileLines) {
-        String headers = csvFileLines.getFirst();
-        String[] validHeaders = {"UniqueID", "Registered Company Name", "Company Number", "Trading Name", "First Name", "Last Name", "Date of Birth"};
+    private Boolean isValidFieldHeaders (CSVRecord record) {
+        List<String>  actualHeaders = record.toList();
+        if (!actualHeaders.equals(VALID_HEADERS)) {
+            LOGGER.error("Headers did not match expected headers");
+            return false;
+        }
+        return true;
+    }
 
-        String[] separatedHeaders = headers.split(",");
 
-        return Arrays.equals(validHeaders, separatedHeaders);
-
+    private boolean isDataAfterHeaders (Iterator<CSVRecord> iterator) {
+        if (!iterator.hasNext()) {
+            LOGGER.error("No records in file, not even headers");
+            return false;
+        }
+        iterator.next();
+        if (!iterator.hasNext()) {
+            LOGGER.error("No records in file after headers");
+            return false;
+        }
+        return true;
     }
 }
