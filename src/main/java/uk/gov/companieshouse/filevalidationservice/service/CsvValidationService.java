3968e00405fcdf8a1d424d083f4a15632bda6508
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-10-18 12:28:13 +0100

    create CVSProcesser to check validity of a file

--- src/main/java/uk/gov/companieshouse/filevalidationservice/service/CsvValidationService.java
+++ src/main/java/uk/gov/companieshouse/filevalidationservice/service/CsvValidationService.java
@@ -14,29 +14,4 @@ import java.util.Scanner;
 public class CsvValidationService {
 
 
-
-    public List<String> openCsvFile (String fileName) throws FileNotFoundException {
-        File importedFile = new File(fileName);
-        Scanner scanner = new Scanner(importedFile);
-        List<String> fileLines = new ArrayList<>();
-
-
-        while (scanner.hasNextLine()) {
-            fileLines.add(scanner.nextLine());
-        }
-
-        return  fileLines;
-    }
-
-
-    public Boolean validateFieldHeaders (List<String> csvFileLines) {
-        String headers = csvFileLines.getFirst();
-        String[] validHeaders = {"UniqueID", "Registered Company Name", "Company Number", "Trading Name", "First Name", "Last Name", "Date of Birth"};
-
-        String[] separatedHeaders = headers.split(",");
-
-        return Arrays.equals(validHeaders, separatedHeaders);
-
-    }
-
 }
