09e880b7b3e7162e0fe742943f6d05939c627b91
hemerem-ch <hemerem@companieshouse.gov.uk>
2024-10-18 12:29:08 +0100

    create CVSProcesser to check validity of a file

--- src/main/java/uk/gov/companieshouse/filevalidationservice/service/CsvValidationService.java
+++ src/main/java/uk/gov/companieshouse/filevalidationservice/service/CsvValidationService.java
@@ -2,13 +2,6 @@ package uk.gov.companieshouse.filevalidationservice.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 @Service
 public class CsvValidationService {
