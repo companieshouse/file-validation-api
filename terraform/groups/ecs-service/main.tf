5bec5bcf03ad0840be11364d23e980f0eecfdc04
akachwala-ch <akachwala@companieshouse.gov.uk>
2024-11-05 14:08:51 +0000

    lb changes for internalapi

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -42,24 +42,13 @@ module "ecs-service" {
   lb_listener_arn                   = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority         = local.lb_listener_rule_priority
   lb_listener_paths                 = local.lb_listener_paths
-  multilb_setup                     = true
-  multilb_listeners                 = {
-    "priv-api-lb": {
-      listener_arn                  = data.aws_lb_listener.secondary_lb_listener.arn,
-      load_balancer_arn             = data.aws_lb.secondary_lb.arn
-    }
-    "pub-api-lb": {
-      load_balancer_arn      = data.aws_lb.service_lb.arn
-      listener_arn           = data.aws_lb_listener.service_lb_listener.arn
-    }
-  }
 
   # Docker container details
   docker_registry   = var.docker_registry
   docker_repo       = local.docker_repo
   container_version = var.file_validation_api_version
   container_port    = local.container_port
-  
+
   # Service configuration
   service_name              = local.service_name
   name_prefix               = local.name_prefix
