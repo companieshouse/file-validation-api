589e40902b5335d57262c8e3919b22054109b52b
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-11-25 11:47:56 +0000

    Updating services and controllers to connect to the new FileTransferApiClient

--- src/main/java/uk/gov/companieshouse/filevalidationservice/service/FileTransferService.java
+++ src/main/java/uk/gov/companieshouse/filevalidationservice/service/FileTransferService.java
@@ -1,73 +1,62 @@
 package uk.gov.companieshouse.filevalidationservice.service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import uk.gov.companieshouse.api.error.ApiErrorResponseException;
-import uk.gov.companieshouse.api.handler.exception.URIValidationException;
-import uk.gov.companieshouse.api.model.ApiResponse;
+import org.springframework.web.client.HttpClientErrorException;
 import uk.gov.companieshouse.api.model.filetransfer.AvStatusApi;
-import uk.gov.companieshouse.api.model.filetransfer.FileApi;
 import uk.gov.companieshouse.api.model.filetransfer.FileDetailsApi;
 import uk.gov.companieshouse.filevalidationservice.exception.RetryException;
 import org.springframework.web.multipart.MultipartFile;
-import uk.gov.companieshouse.api.model.filetransfer.IdApi;
-import uk.gov.companieshouse.filevalidationservice.rest.FileTransferEndpoint;
+import uk.gov.companieshouse.filevalidationservice.rest.FileTransferApiClient;
 import uk.gov.companieshouse.filevalidationservice.utils.StaticPropertyUtil;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
-import java.util.Map;
+import java.io.IOException;
+
 import java.util.Optional;
 
-import java.io.IOException;
 
 @Service
 public class FileTransferService {
 
-    private final FileTransferEndpoint fileTransferEndpoint;
+    private final FileTransferApiClient fileTransferApiClient;
     private final RetryService retryService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger( StaticPropertyUtil.APPLICATION_NAMESPACE );
 
-    public FileTransferService( final FileTransferEndpoint fileTransferEndpoint,
-                                RetryService retryService ) {
-        this.fileTransferEndpoint = fileTransferEndpoint;
+    public FileTransferService(FileTransferApiClient fileTransferApiClient,
+                                RetryService retryService) {
+        this.fileTransferApiClient = fileTransferApiClient;
         this.retryService = retryService;
     }
 
     private Optional<FileDetailsApi> getFileDetails(final String id) {
         try {
-            ApiResponse<FileDetailsApi> response = fileTransferEndpoint.details(id);
-            return Optional.ofNullable(response.getData());
-        } catch ( URIValidationException e) {
-            throw new RuntimeException(e);
-        } catch (ApiErrorResponseException e) {
-            if (e.getStatusCode() == 404) {
+            ResponseEntity<FileDetailsApi> response = fileTransferApiClient.details(id);
+            return Optional.ofNullable(response.getBody());
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
                 return Optional.empty();
             }
-            var message = "Unexpected response status from file transfer api when getting file details.";
-            LOGGER.errorContext(id, message, null, Map.of(
-                    "expected", "200",
-                    "status", e.getStatusCode()
-            ));
-            throw new RuntimeException(e.getMessage());
+            LOGGER.error("Unexpected response status from file transfer api when getting file details.");
+            throw e;
         }
     }
 
-    public Optional<FileApi> get(String id) {
+    public Optional<byte[]> get(String id) {
         Optional<FileDetailsApi> details = retryService.attempt(() -> {
             Optional<FileDetailsApi> maybeFileDetails;
             maybeFileDetails = getFileDetails(id);
             var stillAwaitingScan = maybeFileDetails
                     .map(fileDetailsApi -> fileDetailsApi.getAvStatusApi().equals(AvStatusApi.NOT_SCANNED))
                     .orElse(false);
-
-            LOGGER.debugContext(id, "File still awaiting scan. Retrying.", null);
-
             if (stillAwaitingScan) {
                 // AvScan has still not been completed. Attempt to retry
+                LOGGER.debugContext(id, "File still awaiting scan. Retrying.", null);
                 throw new RetryException();
             }
-
             return maybeFileDetails;
         });
 
@@ -76,17 +65,17 @@ public class FileTransferService {
             return Optional.empty();
         }
         try{
-            ApiResponse<FileApi> response = fileTransferEndpoint.download(id);
-            return Optional.of(response.getData());
-        } catch (ApiErrorResponseException | URIValidationException e) {
-            throw new RuntimeException(e);
+            ResponseEntity<byte[]> response = fileTransferApiClient.download(id);
+            return Optional.ofNullable(response.getBody());
+        } catch (HttpClientErrorException e) {
+            LOGGER.error("Unexpected response status from file transfer api when downloading file.");
+            throw e;
         }
     }
-    public ApiResponse<IdApi> upload(MultipartFile file) {
+    public ResponseEntity<String> upload(MultipartFile file) {
         try {
-            var fileApi = new FileApi(file.getName(),file.getBytes(),"text/csv",(int)file.getSize(),".csv");
-            return fileTransferEndpoint.upload(fileApi);
-        } catch (URIValidationException | IOException e) {
+            return fileTransferApiClient.upload(file);
+        } catch (HttpClientErrorException | IOException e) {
             throw new RuntimeException(e);
         }
     }
