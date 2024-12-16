746eb416d12fa3b33cc763719379668639154311
ttingle-ch <ttingle@companieshouse.gov.uk>
2024-12-16 11:31:15 +0000

    Removed unused files and added snake case naming strategy for Mongo

--- src/main/java/uk/gov/companieshouse/filevalidationservice/configuration/ValidatorConfig.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.companieshouse.filevalidationservice.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ValidatorConfig {
-    // TODO: is this file required?
-}
--- src/main/java/uk/gov/companieshouse/filevalidationservice/service/CsvValidationService.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.companieshouse.filevalidationservice.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class CsvValidationService {
-}
