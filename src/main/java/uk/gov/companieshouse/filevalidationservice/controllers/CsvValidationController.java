5996bf1ce5ca6ca85dcad5501adf383444e5ca04
sardhani <sardhani@companieshouse.gov.uk>
2024-12-18 09:43:44 +0000

    IDVA5-1588 improve validation

--- src/main/java/uk/gov/companieshouse/filevalidationservice/controllers/CsvValidationController.java
+++ src/main/java/uk/gov/companieshouse/filevalidationservice/controllers/CsvValidationController.java
@@ -3,6 +3,7 @@ package uk.gov.companieshouse.filevalidationservice.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -56,6 +57,9 @@ public class CsvValidationController implements FileValidationInterface {
             }
             var objectMapper = new ObjectMapper();
             var fileMetaData = objectMapper.readValue(metadata, FileMetaData.class);
+            if(StringUtils.isEmpty(fileMetaData.getFileName()) || StringUtils.isEmpty(fileMetaData.getFromLocation()) || StringUtils.isEmpty(fileMetaData.getToLocation())){
+                throw new BadRequestRuntimeException("Please provide a valid metadata: " + fileMetaData);
+            }
             String id = fileTransferService.upload(file, fileMetaData);
             return ResponseEntity.created(URI.create(Constants.UPLOAD_URI_PATTERN)).body(new FileUploadResponse().id(id));
         } catch (FileUploadException | JsonProcessingException e) {
