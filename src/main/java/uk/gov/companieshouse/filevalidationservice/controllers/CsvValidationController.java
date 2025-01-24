20493ec5e19ba322ceaae945e83ee54462623f9e
ttingle-ch <ttingle@companieshouse.gov.uk>
2025-01-24 14:53:06 +0000

    Adding Tika to determine the file type

--- src/main/java/uk/gov/companieshouse/filevalidationservice/controllers/CsvValidationController.java
+++ src/main/java/uk/gov/companieshouse/filevalidationservice/controllers/CsvValidationController.java
@@ -1,9 +1,9 @@
 package uk.gov.companieshouse.filevalidationservice.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.tika.Tika;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +20,7 @@ import uk.gov.companieshouse.filevalidationservice.utils.StaticPropertyUtil;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
 
 @RestController
@@ -37,8 +38,14 @@ public class CsvValidationController implements FileValidationInterface {
     public ResponseEntity<FileUploadResponse> uploadFile(MultipartFile file, @Valid String metadata){
         try {
 
-            LOGGER.info(file.toString());
-            LOGGER.info(String.format("Name: %s, Original Filename: %s, Content Type: %s", file.getName(), file.getOriginalFilename(), file.getContentType()));
+//            // checking file extension in file name
+//            if (file.isEmpty() || !file.getOriginalFilename().endsWith(".csv")) {
+//                throw new BadRequestRuntimeException("Please upload a valid CSV file");
+//            }
+            Tika tika = new Tika();
+            String type = tika.detect(file.getInputStream(), file.getOriginalFilename());
+
+            LOGGER.info("File type: " + type);
 
             var objectMapper = new ObjectMapper();
             var fileMetaData = objectMapper.readValue(metadata, FileMetaData.class);
@@ -47,7 +54,7 @@ public class CsvValidationController implements FileValidationInterface {
             }
             String id = fileTransferService.upload(file, fileMetaData);
             return ResponseEntity.created(URI.create(Constants.UPLOAD_URI_PATTERN)).body(new FileUploadResponse().id(id));
-        } catch (FileUploadException | JsonProcessingException e) {
+        } catch (FileUploadException | IOException e) {
             throw new InternalServerErrorRuntimeException(e.getMessage());
         }
     }
